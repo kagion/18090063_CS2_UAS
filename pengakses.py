b68f0d5aac3e4774415c1bee9cebb5a3b08d3b72
kagion <Soegiono1998@gmail.com>
2020-07-16 15:09:16 +0700

    jwt

---
+++ pengakses.py
@@ -0,0 +1,20 @@
+from app import *
+
+
+@app.route('/getusers', methods=['GET'])
+@jwt_required
+def getusers():
+    all_users = Mhs.get_all_users()
+    result = users_schema.dump(all_users)
+    return jsonify(result), 200
+
+
+@app.route('/getuser/<id>', methods=['GET'])
+@jwt_required
+def getuser(id):
+    mahasiswa = Mhs.query.get(id)
+    return user_schema.jsonify(mahasiswa)
+
+
+if __name__ == '__main__':
+    app.run()
